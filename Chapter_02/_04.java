ad60a242b4a1a3652e55543affe04a86b317a177
mohamed-nazmi <mohamednazmi96@gmail.com>
2019-09-13 23:46:12 +0200

    2.4 - Partition

---
+++ Chapter_02/_04.java
@@ -0,0 +1,25 @@
+package Chapter_02;
+
+public class _04 {
+    public static LinkedListNode partition(LinkedListNode node, int x) {
+        LinkedListNode head = node;
+        LinkedListNode tail = node;
+
+        while (node != null) {
+            LinkedListNode next = node.next;
+            if (node.data < x) {
+                node.next = head;
+                head = node;
+            } else {
+                tail.next = node;
+                tail = node;
+            }
+            node = next;
+        }
+        
+        if (tail != null)
+            tail.next = null;
+
+        return head;
+    }
+}
