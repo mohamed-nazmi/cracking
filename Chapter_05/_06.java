1d4446759bfd2aab45b24d757851b7afaae28937
mohamed-nazmi <mohamednazmi96@gmail.com>
2019-11-21 12:17:14 +0200

    5.6 - Conversion

---
+++ Chapter_05/_06.java
@@ -0,0 +1,11 @@
+package Chapter_05;
+
+public class _06 {
+    public static int flipBits(int a, int b) {
+        int count = 0;
+        for (int c = a ^ b; c != 0; c = c >> 1) {
+            count += c & 1;
+        }
+        return count;
+    }
+}
